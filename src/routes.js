0d8176209955303186a8e1f0f6620f82933cb7ea
Weverson Diazz <wevers.info@gmail.com>
2020-10-29 16:02:30 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", function(req, res) {
-    return res.send("Page Landing");
+    return res.render("layout.njk");
 });
 
 module.exports = routes;
