a410d3436f33c6f5ae82d3e985083bbd549064e9
Weverson Diazz <wevers.info@gmail.com>
2020-11-01 21:17:22 -0300

    Update Category.js

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -3,8 +3,8 @@ const db = require("../../config/db");
 module.exports = {
 
     all() {
-        db.query(`
-            SELECT * FROM cetegories
+        return db.query(`
+            SELECT * FROM categories
         `);
     }
 }
