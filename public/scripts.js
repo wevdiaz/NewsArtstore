c97a5935249d6050b3cddee6efe3195f5edebcbb
Weverson Diazz <wevers.info@gmail.com>
2020-11-01 22:38:48 -0300

    Create scripts.js
    expressão regular no campo preço

---
+++ public/scripts.js
@@ -0,0 +1,13 @@
+const input = document.querySelector("input[name='price']");
+
+input.addEventListener("keydown", function(e){
+    
+    setTimeout(function() {
+        let { value } = e.target;
+
+        value = value.replace(/\D/g, "");
+
+        e.target.value = value;
+
+    }, 1);
+})
