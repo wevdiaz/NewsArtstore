2c9292d3c93ebebd688d64608f10e83e13766371
Weverson Diazz <wevers.info@gmail.com>
2020-11-12 16:15:55 -0300

    Update multer.js
    caminho para armazenar o arquivo de foto no disco

--- src/app/middlewares/multer.js
+++ src/app/middlewares/multer.js
@@ -1,3 +1,18 @@
 const multer = require("multer");
 
-module.exports = multer({});
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now().toString()}-${file.originalname}`);
+    }
+});
+
+
+
+
+module.exports = multer({
+    storage,
+    fileFilter
+});
