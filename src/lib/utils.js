ab29747b629387d9b30262fdae8819e3b8860447
Weverson Diazz <wevers.info@gmail.com>
2020-11-18 12:23:26 -0300

    Update utils.js
    função com formatação para datas e horas

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,5 +5,27 @@ module.exports = {
             style: "currency",
             currency: "BRL"
         }).format(price/100);
+    },
+
+    date(timestamp) {
+        const date = new Date(timestamp);
+
+        const year = date.getUTCFullYear();
+        const month = `0${date.getUTCMonth()}`.slice(-2);
+        const day = `0${date.getUTCDate()}`.slice(-2);
+
+        const hour = date.getHours();
+        const minutes = date.getMinutes();
+
+        return {
+            day,
+            month,
+            year,
+            hour,
+            minutes,
+            iso: `${year}-${month}-${day}`,
+            birthDay: `${day}/${month}`,
+            format: `${day}/${month}/${year}`
+        }
     }
 }
