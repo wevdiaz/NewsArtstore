243b62ec8b3be8c98ccd41877aa8a9d9a55159f7
Weverson Diazz <wevers.info@gmail.com>
2020-11-29 14:34:11 -0300

    Update HomeController.js
    tratando os dados da Home

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -13,14 +13,20 @@ module.exports = {
 
         async function getImage(productId) {
             let results = await Product.files(productId);
-            const files = results.rows.map(file => ({
-                ...file,
-                src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
-            }));
+            const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`);
 
             return files[0];
         }
 
-        
+        const productsPromise = products.map(product => {
+            product.img = await getImage(product.id);
+            product.oldPrice = formatPrice(product.old_price);
+            product.price = formatPrice(product.price);
+            return product;
+        });
+
+        const lastAdded = await Promise.all(productsPromise);
+
+        return res.render("home/index", { products: lastAdded });
     }
 }
