06cd6188edc98c4e76d911cfc842818f38d9c6ac
Weverson Diazz <wevers.info@gmail.com>
2020-12-05 17:30:40 -0300

    Update SearchController.js
    Preparando o filtro da página

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -6,27 +6,24 @@ const Product = require("../models/Product");
 module.exports = {
 
     async index(req, res) {
-        let results = await Product.all();
-        const products = results.rows;
-
-        if (!products) return res.send("Products not found!");
-
-        async function getImage(productId) {
-            let results = await Product.files(productId);
-            const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`);
-
-            return files[0];
-        }
-
-        const productsPromise = products.map(async product => {
-            product.img = await getImage(product.id);
-            product.oldPrice = formatPrice(product.old_price);
-            product.price = formatPrice(product.price);
-            return product;
-        }).filter((product, index) => index > 2 ? false : true);
-
-        const lastAdded = await Promise.all(productsPromise);
         
-        return res.render("search/index", { products: lastAdded });
+        try {
+
+            let results,
+                params = {};
+
+
+            const { filter, category } = req.query;
+
+            if (!filter) return res.redirect("/");
+
+            params.filter = filter;
+
+
+            return res.render("search/index", { products });
+
+        }catch(err) {
+            console.error(err);
+        }
     }
 }
