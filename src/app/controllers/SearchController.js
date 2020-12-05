c869aece92a5ed3887cd72d0c2579b12643637ac
Weverson Diazz <wevers.info@gmail.com>
2020-12-05 18:08:26 -0300

    Fazendo o código de filtragem no controller

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -25,6 +25,27 @@ module.exports = {
 
             results = await Product.search(params);
 
+            async function getImage(productId) {
+                let results = await Product.files(productId);
+                const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`);
+    
+                return files[0];
+            }
+
+            const productsPromise = results.rows.map(async product => {
+                product.img = await getImage(product.id);
+                product.oldPrice = formatPrice(product.old_price);
+                product.price = formatPrice(product.price);
+                return product;
+            });
+
+            const products = await Promise.all(productsPromise);
+
+            const search = {
+                term: req.query.filter,
+                total: products.length
+            }
+
 
             return res.render("search/index", { products });
 
