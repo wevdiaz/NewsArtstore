d20e2831c19970fea6b6dba0fbb1b1fed0644b4c
Weverson Diazz <wevers.info@gmail.com>
2020-12-21 13:29:06 -0300

    Estrutura das rotas da seção e controle de usuários

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,20 +1,29 @@
-// login/logout
-routes.get("/login", SessionController.loginForm );
-routes.post("/login", SessionController.login );
-routes.post("/logout", SessionController.logout );
+const express = require("express");
+const routes = express.Router();
+
+const SessionController = require("../app/controllers/SessionController");
+const UserController = require("../app/controllers/UserController");
+
+// // login/logout
+// routes.get("/login", SessionController.loginForm );
+// routes.post("/login", SessionController.login );
+// routes.post("/logout", SessionController.logout );
 
 
-// reset password / forgot
-routes.get("/forgot-password", SessionController.forgotForm);
-routes.get("/password-reset", SessionController.resetForm );
-routes.post("/forgot-password", SessionController.forgot );
-routes.post("/password-reset", SessionController.reset );
+// // reset password / forgot
+// routes.get("/forgot-password", SessionController.forgotForm);
+// routes.get("/password-reset", SessionController.resetForm );
+// routes.post("/forgot-password", SessionController.forgot );
+// routes.post("/password-reset", SessionController.reset );
 
 
-// user register UserController
-routes.get("/register", UserController.registerForm );
-routes.post("/register", UserController.post );
+// // user register UserController
+// routes.get("/register", UserController.registerForm );
+// routes.post("/register", UserController.post );
 
-routes.get("/", UserController.show );
-routes.put("/", UserController.update );
-routes.delete("/", UserController.delete );
+// routes.get("/", UserController.show );
+// routes.put("/", UserController.update );
+// routes.delete("/", UserController.delete );
+
+
+module.exports = routes;
