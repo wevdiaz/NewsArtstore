4e44b701e338e36743761698e7c3427cfcdab9e0
Weverson Diazz <wevers.info@gmail.com>
2020-12-27 13:26:55 -0300

    limitando a máscara

--- public/scripts.js
+++ public/scripts.js
@@ -21,14 +21,15 @@ const Mask = {
 
         value = value.replace(/\D/g, "");
 
+        if (value.length > 14) {
+            value = value.slice(0, -1);
+        }
+
         if (value.length > 11) {
 
             value = value.replace(/(\d{2})(\d)/, "$1.$2");
-
             value = value.replace(/(\d{3})(\d)/, "$1.$2");
-
             value = value.replace(/(\d{3})(\d)/, "$1/$2");
-
             value = value.replace(/(\d{4})(\d)/, "$1-$2");
 
         }
