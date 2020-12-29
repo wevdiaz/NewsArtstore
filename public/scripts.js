b6c5dc37d5b18f45058a719823cf05fc85626925
Weverson Diazz <wevers.info@gmail.com>
2020-12-29 16:02:38 -0300

    expressão regular para validar email

--- public/scripts.js
+++ public/scripts.js
@@ -238,6 +238,7 @@ const Validate = {
 
     isEmail(value) {
         let error = null;
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
         return {
             error,
