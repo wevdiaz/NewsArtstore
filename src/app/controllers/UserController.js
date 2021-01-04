0cbab655e2cce16248a7c13b32d11b457495a5f4
Weverson Diazz <wevers.info@gmail.com>
2021-01-04 13:08:28 -0300

    verificando campos e checando email e cpf ou cnpj para validação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,25 @@
-
+const User = require("../models/User");
 
 module.exports = {
 
     registerForm(req, res) {
         return res.render("user/register");
+    },
+
+    post(req, res) {
+        const keys = Object.keys(req.body);
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send("Please, fill all fields! ");
+            }
+        }
+
+        const { email, cpf_cnpj } = req.body;
+
+        const user = await User.findOne({
+            where: { email },
+            or: { cpf_cnpj }
+        });
     }
 }
