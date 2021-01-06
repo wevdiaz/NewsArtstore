7b432784a8e0658acb6f6913287dbe02d7c9157f
Weverson Diazz <wevers.info@gmail.com>
2021-01-06 14:45:50 -0300

    organizando as validações em arquivo externo

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,29 +7,7 @@ module.exports = {
     },
 
     async post(req, res) {
-        const keys = Object.keys(req.body);
-
-        for (key of keys) {
-            if (req.body[key] == "") {
-                return res.send("Please, fill all fields! ");
-            }
-        }
-
-        let { email, cpf_cnpj, password, passwordRepeat } = req.body;  
         
-        cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
-
-        const user = await User.findOne({
-            where: { email },
-            or: { cpf_cnpj }
-        });
-
-        if (user) return res.send("Users Exists");
-
-        if (password != passwordRepeat) {
-            return res.send("Password Mismatch")
-        }
-
         return res.send('Passed');
     }
 }
