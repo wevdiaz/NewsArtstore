5003997f0d9c229448d3189869a972f8aa8f8df3
Weverson Diazz <wevers.info@gmail.com>
2021-01-17 13:44:41 -0300

    controller de atualização do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -26,5 +26,32 @@ module.exports = {
         return res.redirect('/users');
     },
 
-    async update(req, res, next) {}
+    async update(req, res, next) {
+
+        try {
+            let { name, email, cpf_cnpj, cep, address } = req.body;
+
+            cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
+            cep = cep.replace(/\D/g, "");
+
+            await User.update(user.id, {
+                name,
+                email,
+                cpf_cnpj,
+                cep,
+                address
+            });
+
+            return res.render("user/index", {
+                success: "conta atualizada com sucesso!"
+            });
+
+
+        }catch(err) {
+            console.error(err);
+            return res.render("user/index", {
+                error: "Algum erro aconteceu!"
+            });
+        }
+    }
 }
