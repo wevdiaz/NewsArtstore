7af8d55b937b0f13ec7d35ceee4d792e50f24299
Weverson Diazz <wevers.info@gmail.com>
2021-01-23 14:47:38 -0300

    rota para login e logout

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,7 @@ const UserController = require("../app/controllers/UserController");
 const Validator = require("../app/validators/user");
 
 // // login/logout
-// routes.get("/login", SessionController.loginForm );
+routes.get("/login", SessionController.loginForm );
 // routes.post("/login", SessionController.login );
 routes.post("/logout", SessionController.logout );
 
