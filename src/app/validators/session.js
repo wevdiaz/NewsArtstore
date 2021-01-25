b672f6278913defa38a320188275a51edb5ee441
Weverson Diazz <wevers.info@gmail.com>
2021-01-25 15:22:08 -0300

    correção

--- src/app/validators/session.js
+++ src/app/validators/session.js
@@ -24,34 +24,6 @@ async function login(req, res, next) {
     next();
 }
 
-async function post(req, res, next) {
-
-        const fillAllFields = checkAllFields(req.body);
-        if (fillAllFields) {
-            return res.render("user/register", fillAllFields);
-        }
-
-        let { email, cpf_cnpj, password, passwordRepeat } = req.body;  
-        
-        cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
-
-        const user = await User.findOne({
-            where: { email },
-            or: { cpf_cnpj }
-        });
-
-        if (user) return res.render("user/register", {
-            user: req.body,
-            error: "Usuário já cadastrado"
-        });
-
-        if (password != passwordRepeat) return res.render("user/register", {
-            user: req.body,
-            error: "A senha e a repetição estão diferentes."
-        });
-
-    next();
-}
 
 module.exports = {
     login
