e6f498e2f97d0797fcea66599875b3c45038b67b
Weverson Diazz <wevers.info@gmail.com>
2021-02-01 15:54:03 -0300

    rota do formulário de recuperação

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -13,5 +13,11 @@ module.exports = {
     logout(req, res) {
         req.session.destroy();
         return res.redirect("/");
-    }
+    },
+
+    forgotForm(req, res) {
+        return res.render("session/forgot-password");
+    },
+
+    forgot(req, res) {}
 }
