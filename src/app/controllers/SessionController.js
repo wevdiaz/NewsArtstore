b4dc7318645e615b472b61ef6a382271a07a8387
Weverson Diazz <wevers.info@gmail.com>
2021-02-03 19:23:50 -0300

    estrutura para envio de email com nodemailer

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,6 @@
 const crypto = require("crypto");
+const mailer = require("../../lib/mailer");
+
 const User = require("../models/User");
 
 module.exports = {
@@ -33,6 +35,25 @@ module.exports = {
         await User.update(user.id, {
             reset_token: token,
             reset_token_expires: now
+        });
+
+
+        await mailer.sendMail({
+            to: user.email,
+            from: "no-reply@newsartstore.com.br",
+            subject: "Recuperação de Senha",
+            html: `<h2>Perdeu a chave ?</h2>
+            <p>Não se preocupe, clique no link abaixo para recuperar sua senha</p>
+            <p>
+                <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
+                    RECUPERAR SENHA
+                </a>
+            </p>
+            `
+        });
+
+        return res.render("session/forgot-password", {
+            success: "Verifique seu email para resetar sua senha"
         });
     }
 }
