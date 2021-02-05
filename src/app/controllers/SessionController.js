d134ff3e0246c4bd4fbd03df889b51cebb0a1c5e
Weverson Diazz <wevers.info@gmail.com>
2021-02-04 22:13:27 -0300

    corrigindo bugs do pedido de recuperação de senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -24,36 +24,46 @@ module.exports = {
         return res.render("session/forgot-password");
     },
 
-    forgot(req, res) {
+    async forgot(req, res) {
         const user = req.user;
 
-        const token = crypto.randomBytes(20).toString("hex");
+        try {
 
-        let now = new Date();
-        now = now.setHours(now.getHours() + 1);
+            const token = crypto.randomBytes(20).toString("hex");
 
-        await User.update(user.id, {
-            reset_token: token,
-            reset_token_expires: now
-        });
+            let now = new Date();
+            now = now.setHours(now.getHours() + 1);
+
+            await User.update(user.id, {
+                reset_token: token,
+                reset_token_expires: now
+            });
 
 
-        await mailer.sendMail({
-            to: user.email,
-            from: "no-reply@newsartstore.com.br",
-            subject: "Recuperação de Senha",
-            html: `<h2>Perdeu a chave ?</h2>
-            <p>Não se preocupe, clique no link abaixo para recuperar sua senha</p>
-            <p>
-                <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
-                    RECUPERAR SENHA
-                </a>
-            </p>
-            `
-        });
+            await mailer.sendMail({
+                to: user.email,
+                from: "no-reply@newsartstore.com.br",
+                subject: "Recuperação de Senha",
+                html: `<h2>Perdeu a chave ?</h2>
+                <p>Não se preocupe, clique no link abaixo para recuperar sua senha</p>
+                <p>
+                    <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
+                        RECUPERAR SENHA
+                    </a>
+                </p>
+                `
+            });
 
-        return res.render("session/forgot-password", {
-            success: "Verifique seu email para resetar sua senha"
-        });
+            return res.render("session/forgot-password", {
+                success: "Verifique seu email para resetar sua senha"
+            });
+
+        }catch(err) {
+            console.error(err);
+            return res.render("session/forgot-password", {
+                error: "Erro inesperado, tente novamente"
+            });
+        }
+        
     }
 }
