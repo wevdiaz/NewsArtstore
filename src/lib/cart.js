8fb71da3c30c7580889eaedd70b3872a3bf44471
Weverson Diazz <wevers.info@gmail.com>
2021-06-15 19:39:58 -0300

    Trabalhando com a lógica de adicionar itens no carrinho

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -20,11 +20,69 @@ const Cart = {
         return this;
     },
 
-    addOne(product) {},
+    addOne(product) {
+        let inCart = this.items.find( item => item.product.id == product.id );
+
+        if (!inCart) {
+            inCart = {
+                product: {
+                    ...product,
+                    formattedPrice: formatPrice(product.price)
+                },
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0)
+            }
+
+            this.items.push(inCart);
+        }
+
+        if (inCart.quantity >= product.quantity) return this;
+
+        // Update item
+        inCart.quantity++;
+        inCart.price = inCart.product.price * inCart.quantity;
+        inCart.formattedPrice = formatPrice(inCart.price);
+
+        // Update Cart
+        this.total.quantity++;
+        this.total.price += inCart.product.price;
+        this.total.formattedPrice = formatPrice(this.total.price);
+
+        return this;
+    },
 
     removeOne(productId) {},
 
     delete(productId) {}
 }
+
+const product = {
+    id: 1,
+    price: 199,
+    quantity: 2
+}
+
+const product2 = {
+    id: 2,
+    price: 229,
+    quantity: 3
+}
+
+console.log("Primeiro add")
+let oldCart = Cart.init().addOne(product);
+console.log(oldCart);
+
+console.log("segundo add")
+Cart.init(oldCart).addOne(product);
+console.log(oldCart);
+
+console.log("terceiro add")
+Cart.init(oldCart).addOne(product2);
+console.log(oldCart);
+
+console.log("last add")
+Cart.init(oldCart).addOne(product);
+console.log(oldCart);
 
 module.exports = Cart;
