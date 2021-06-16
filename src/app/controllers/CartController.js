f6c9a08e1d05d59d181eb3b2075efa8cfcfc8c2c
Weverson Diazz <wevers.info@gmail.com>
2021-06-16 12:43:33 -0300

    Montando a estrutura HTML do Carrinho

---
+++ src/app/controllers/CartController.js
@@ -0,0 +1,20 @@
+const Cart = require("../../lib/cart");
+
+const LoadProductService = require("../services/LoadProductService");
+
+module.exports = {
+
+    async index(req, res) {
+        try {
+            const product = await LoadProductService.load("product", { where: { id: 14}});
+            let { cart } = req.session;
+
+            cart = Cart.init(cart).addOne(product);
+
+            return res.render("cart/index", { cart });
+
+        }catch(err) {
+            console.error(err);
+        }
+    }
+}
