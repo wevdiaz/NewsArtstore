b219b9e50c86e17c63ada481172f98e5c72170c1
Weverson Diazz <wevers.info@gmail.com>
2021-06-16 11:36:53 -0300

    configurando o nodemailer

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -4,8 +4,8 @@ const transport = nodemailer.createTransport({
   host: "smtp.mailtrap.io",
   port: 2525,
   auth: {
-    user: "0b5fd2001b7fb9",
-    pass: "2163e949db13fb"
+    user: "24976bb93d53b0",
+    pass: "95af01d8618da4"
   }
 });
 
