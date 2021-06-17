fd476d6cc9c973e3941501edb5db465cd7f7655f
Weverson Diazz <wevers.info@gmail.com>
2021-06-17 11:57:48 -0300

    Utilizando o adicionar item no carrinho

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -1,3 +1,4 @@
+const { addOne } = require("../../lib/cart");
 const Cart = require("../../lib/cart");
 
 const LoadProductService = require("../services/LoadProductService");
@@ -6,15 +7,28 @@ module.exports = {
 
     async index(req, res) {
         try {
-            const product = await LoadProductService.load("product", { where: { id: 14}});
             let { cart } = req.session;
 
-            cart = Cart.init(cart).addOne(product);
+            cart = Cart.init(cart);
 
             return res.render("cart/index", { cart });
 
         }catch(err) {
             console.error(err);
         }
+    },
+
+    async addOne(req, res) {
+        const { id } = req.params;
+
+        const product = await LoadProductService.load("product", { where: { id }});
+
+        let { cart } = req.session;
+
+        cart = Cart.init(cart).addOne(product);
+
+        req.session.cart = cart;
+
+        return res.redirect("/cart");
     }
 }
