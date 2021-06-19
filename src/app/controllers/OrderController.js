79e7cc83c4ff636c89c85535de9cf36f0a41b522
Weverson Diazz <wevers.info@gmail.com>
2021-06-19 20:48:54 -0300

    Criando o index e configurando dados do pedido

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 const Order = require("../models/Order");
 const mailer = require("../../lib/mailer");
 const Cart = require("../../lib/cart");
+const { formatPrice, date } = require("../../lib/utils");
 
 const email = (seller, product, buyer) => `
     <h2>Olá ${seller.name}</h2>
@@ -22,6 +23,38 @@ const email = (seller, product, buyer) => `
 `
 
 module.exports = {
+
+    async index(req, res) {
+        let orders = await Order.findAll({ where: { buyer_id: req.session.userId }});
+
+        const getOrdersPromise = orders.map( async order => {
+            order.product = await LoadProductService.load("products", { where: { id: order.product_id }});
+
+            order.buyer = await User.findOne( { where: { id: order.buyer_id }});
+
+            order.seller = await User.findOne( { where: { id: order.seller_id }});
+
+            order.formattedPrice = formatPrice(order.price);
+            order.formattedTotal = formatPrice(order.total);
+
+            const statuses = {
+                open: "Aberto",
+                sold: "Vendido",
+                canceled: "Cancelado"
+            }
+
+            order.formattedStatus = statuses[order.status];
+
+            const updatedAt = date(order.updated_at);
+            order.formattedUpdatedAt = `${order.formattedStatus} em ${updatedAt.day}/${updatedAt.month}/${updatedAt.year} às ${updatedAt.hour}h ${updatedAt.minutes}`;
+
+            return order;
+        });
+
+        orders = await Promise.all(getOrdersPromise);
+
+        return res.render("orders/index", { orders });
+    },
 
     async post(req,res) {
         try {
@@ -61,6 +94,9 @@ module.exports = {
             }); 
             
             await Promise.all(createOrdersPromise);
+
+            delete req.session.cart;
+            Cart.init();
 
             return res.render("orders/success");
 
